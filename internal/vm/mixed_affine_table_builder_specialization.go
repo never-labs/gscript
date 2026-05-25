238b8fc03efcd093bacb97e78cb0106c6676e081
jxwr <jxwr.cn@gmail.com>
2026-05-25 08:12:15 +0800

    vm: use pattern matchers for mixed table builders

--- internal/vm/mixed_affine_table_builder_specialization.go
+++ internal/vm/mixed_affine_table_builder_specialization.go
@@ -30,40 +30,47 @@ func mixedAffineTableBuilderSpecForProto(p *FuncProto) (mixedAffineTableBuilderS
 		return spec, false
 	}
 	code := p.Code
-	if DecodeOp(code[0]) != OP_NEWTABLE ||
-		DecodeOp(code[1]) != OP_LOADINT || DecodeA(code[1]) != 2 || DecodesBx(code[1]) != 1 ||
-		DecodeOp(code[2]) != OP_MOVE || DecodeA(code[2]) != 3 || DecodeB(code[2]) != 0 ||
-		DecodeOp(code[3]) != OP_LOADINT || DecodeA(code[3]) != 4 || DecodesBx(code[3]) != 1 ||
-		DecodeOp(code[4]) != OP_FORPREP ||
-		DecodeOp(code[5]) != OP_LOADINT ||
-		DecodeOp(code[6]) != OP_MUL ||
-		DecodeOp(code[7]) != OP_LOADINT ||
-		DecodeOp(code[8]) != OP_ADD ||
-		DecodeOp(code[10]) != OP_SETTABLE ||
-		DecodeOp(code[11]) != OP_LOADINT ||
-		DecodeOp(code[12]) != OP_MOD ||
-		DecodeOp(code[13]) != OP_LOADINT || DecodesBx(code[13]) != 0 ||
-		DecodeOp(code[14]) != OP_EQ ||
-		DecodeOp(code[16]) != OP_LOADINT ||
-		DecodeOp(code[17]) != OP_MUL ||
-		DecodeOp(code[18]) != OP_LOADINT ||
-		DecodeOp(code[19]) != OP_ADD ||
-		DecodeOp(code[20]) != OP_LOADK || DecodeBx(code[20]) != 0 ||
-		DecodeOp(code[22]) != OP_CONCAT ||
-		DecodeOp(code[23]) != OP_SETTABLE ||
-		DecodeOp(code[24]) != OP_LOADINT ||
-		DecodeOp(code[25]) != OP_MOD ||
-		DecodeOp(code[26]) != OP_LOADINT || DecodesBx(code[26]) != 0 ||
-		DecodeOp(code[27]) != OP_EQ ||
-		DecodeOp(code[29]) != OP_LOADINT ||
-		DecodeOp(code[30]) != OP_MUL ||
-		DecodeOp(code[31]) != OP_LOADINT ||
-		DecodeOp(code[32]) != OP_ADD ||
-		DecodeOp(code[34]) != OP_UNM ||
-		DecodeOp(code[35]) != OP_SETTABLE ||
-		DecodeOp(code[36]) != OP_FORLOOP ||
-		DecodeOp(code[37]) != OP_MOVE ||
-		DecodeOp(code[38]) != OP_RETURN {
+	pat := newBytecodePattern(code)
+	if !pat.hasOps(
+		opcodeAt{pc: 0, op: OP_NEWTABLE},
+		opcodeAt{pc: 4, op: OP_FORPREP},
+		opcodeAt{pc: 5, op: OP_LOADINT},
+		opcodeAt{pc: 6, op: OP_MUL},
+		opcodeAt{pc: 7, op: OP_LOADINT},
+		opcodeAt{pc: 8, op: OP_ADD},
+		opcodeAt{pc: 10, op: OP_SETTABLE},
+		opcodeAt{pc: 11, op: OP_LOADINT},
+		opcodeAt{pc: 12, op: OP_MOD},
+		opcodeAt{pc: 14, op: OP_EQ},
+		opcodeAt{pc: 16, op: OP_LOADINT},
+		opcodeAt{pc: 17, op: OP_MUL},
+		opcodeAt{pc: 18, op: OP_LOADINT},
+		opcodeAt{pc: 19, op: OP_ADD},
+		opcodeAt{pc: 22, op: OP_CONCAT},
+		opcodeAt{pc: 23, op: OP_SETTABLE},
+		opcodeAt{pc: 24, op: OP_LOADINT},
+		opcodeAt{pc: 25, op: OP_MOD},
+		opcodeAt{pc: 27, op: OP_EQ},
+		opcodeAt{pc: 29, op: OP_LOADINT},
+		opcodeAt{pc: 30, op: OP_MUL},
+		opcodeAt{pc: 31, op: OP_LOADINT},
+		opcodeAt{pc: 32, op: OP_ADD},
+		opcodeAt{pc: 34, op: OP_UNM},
+		opcodeAt{pc: 35, op: OP_SETTABLE},
+		opcodeAt{pc: 36, op: OP_FORLOOP},
+		opcodeAt{pc: 37, op: OP_MOVE},
+		opcodeAt{pc: 38, op: OP_RETURN},
+	) ||
+		!pat.hasASBxs(
+			asbxAt{pc: 1, op: OP_LOADINT, a: 2, sbx: 1},
+			asbxAt{pc: 3, op: OP_LOADINT, a: 4, sbx: 1},
+		) ||
+		!pat.hasABs(abAt{pc: 2, op: OP_MOVE, a: 3, b: 0}) ||
+		!pat.hasBxs(bxAt{pc: 20, op: OP_LOADK, bx: 0}) ||
+		!pat.hasSBxs(
+			sbxAt{pc: 13, op: OP_LOADINT, sbx: 0},
+			sbxAt{pc: 26, op: OP_LOADINT, sbx: 0},
+		) {
 		return spec, false
 	}
 	spec = mixedAffineTableBuilderSpec{
